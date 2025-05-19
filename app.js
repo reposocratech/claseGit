1f9421cae13da8657f61b510809fb8f8daadfc0d
Santiago Pena <santiagopenrod@gmail.com>
2025-05-19 17:39:21 +0200

    cambios

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ if(1==1){
     console.log("hola");
     console.log("que pasa");
     console.log("que tyarde es");
+    for(let i = 0; i<10; i++){
+        console.log(i);
+        
+    }
 }
--- index.js
+++ index.js
@@ -2,4 +2,7 @@ console.log("hola mundo");
 
 let a = 20;
 let res = a +30;
+console.log("hola");
+console.log("pepe");
+
 console.log(res);
