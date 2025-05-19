fc0363a0ee27be126d5716ab4008d81aab3a33aa
adrian <adrian.sanchezJ92@gmail.com>
2025-05-19 17:57:43 +0200

    se lia

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-console.log("Hola App");
-if(1==1){
-    console.log("hola");
-    
+
+console.log('Hola App');
+if (1 == 1) {
+  console.log('hola');
+  console.log("un pinturillo???")
+  console.log("hola, esto es una prueba desde la rama de javi");
+   console.log("que pasa");
+   console.log("que tyarde es");
+    console.log("hola soy miriam");
+    console.log("estoy haciendo cambios")
+   for(let i = 0; i<10; i++){
+        console.log(i);
+        
+    }
 }else if (1==2){
     console.log("adios");
     
